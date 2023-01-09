d4238f4e6c0a5eaa912c56804518f47b81d34683
Dylan Villanueva <121144716+jupetta29@users.noreply.github.com>
2023-01-09 15:14:58 -0600

    Update scoreboard.py high score tracking path

--- scoreboard.py
+++ scoreboard.py
@@ -9,7 +9,7 @@ class Scoreboard(turtle.Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        with open('data.txt', mode='r') as data:
+        with open('./data.txt', mode='r') as data:
             self.high_score = int(data.read())
         self.color('white')
         self.hideturtle()
@@ -24,7 +24,7 @@ class Scoreboard(turtle.Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
-            with open('data.txt', mode='w') as data:
+            with open('./data.txt', mode='w') as data:
                 data.write(f'{self.high_score}')
         self.score = 0
         self.update_scoreboard()
