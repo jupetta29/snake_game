dfb2564ec625eb17ecbb2c8c373fc58ad8793f7f
Dylan Villanueva <121144716+jupetta29@users.noreply.github.com>
2023-01-09 14:28:10 -0600

    Update main.py for high score tracking

--- main.py
+++ main.py
@@ -41,13 +41,12 @@ while game_is_on:
 
     # Detect Collision with Wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        scoreboard.game_over()
-        game_is_on = False
+        scoreboard.reset()
+        snake.reset()
 
     # Detect Collision with Tail
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+            scoreboard.reset()
 
 screen.exitonclick()
