44b93d6e3de0f8d2f9c28d1abada11dc36cd8a90
Dylan Villanueva <121144716+jupetta29@users.noreply.github.com>
2023-01-09 14:28:58 -0600

    Update scoreboard.py for high score tracking

--- scoreboard.py
+++ scoreboard.py
@@ -9,6 +9,8 @@ class Scoreboard(turtle.Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open('data.txt', mode='r') as data:
+            self.high_score = int(data.read())
         self.color('white')
         self.hideturtle()
         self.penup()
@@ -16,13 +18,17 @@ class Scoreboard(turtle.Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(arg=f'Score: {self.score} ', align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(arg=f'Score: {self.score} High Score: {self.high_score}', align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write(arg='GAME OVER', align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open('data.txt', mode='w') as data:
+                data.write(f'{self.high_score}')
+        self.score = 0
+        self.update_scoreboard()
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
